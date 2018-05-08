16148b7b68f1250e81b370be75f2d075154ba47e
Johan Eriksson <jleriksson90@gmail.com>
2018-05-08 19:01:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# "Design Quotes and Inspiration" Slide Presentation
+
+Create presentations using Gatsby, React & Markdown. Inspired by Guillermo Rauch’s [deck on Next.js](https://deck.now.sh/).
+
+* [Demo](//gatsby-deck.netlify.com)
---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    name: `⚡`,
+    title: `Design Slides`,
+    date: `Written on May 8, 2018`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-smartypants`]
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `slides`,
+        path: `${__dirname}/src/pages/`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-postcss-sass`,
+      options: {
+        postCssPlugins: [],
+        precision: 8
+      }
+    }
+  ]
+};
